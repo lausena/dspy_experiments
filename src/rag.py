86e4cf35a3ff9e2547fc7c0d773a0f6843a869d7
lausena <lau.sena.gabriel@gmail.com>
2025-07-28 16:42:40 -0400

    playground

--- src/rag.py
+++ src/rag.py
@@ -1,5 +1,14 @@
+import dspy
+from datasets import load_dataset
+from dspy.datasets import DataLoader
 from src.logger import logger
+import random
 
 def run_rag():
-    print('testing.')
     logger.info('Starting RAG')
+    llama70b = dspy.LM('meta-llama/Llama-3.3-70B-Instruct')
+
+    dspy.configure(lm=llama70b)
+    # kwargs = dict(fields=("claim", "supporting_facts", "hpqa_id", "num_hops"), input_keys=("claim",))
+    # hover = DataLoader().from_huggingface(dataset_name="hover-nlp/hover", split="train", **kwargs)
+
